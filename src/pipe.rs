5a88e12f63e0589c562990f6213ffdeeb601970c
Krystian Bajno <krystian.bajno@gmail.com>
2024-09-01 06:21:08 +0200

    compression

--- src/pipe.rs
+++ src/pipe.rs
@@ -1,9 +1,11 @@
 use std::io;
 
 pub fn encrypt_data_via_pipe(input: &[u8], password: &str) -> Result<Vec<u8>, io::Error> {
-    super::crypto::encrypt_data(input, password).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))
+    let compressed = super::compression::compress_data(input).unwrap();
+    super::crypto::encrypt_data(&compressed, password).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))
 }
 
 pub fn decrypt_data_via_pipe(input: &[u8], password: &str) -> Result<Vec<u8>, io::Error> {
-    super::crypto::decrypt_data(input, password).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))
+    let decrypted = super::crypto::decrypt_data(input, password).map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()));
+    super::compression::decompress_data(&decrypted.unwrap())
 }
